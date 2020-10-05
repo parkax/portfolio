9f9cc2dd08ecc93ecc66d67e868efe0f343f504a
parkax <65437709+parkax@users.noreply.github.com>
2020-10-05 19:15:37 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,115 +2,123 @@ function goBack() {
     window.history.back();
   }
 
- 
+$(document).ready(function() {
+    $( "#az" ).mouseover(function(){
+        $(this).attr("src", "/images/work/grid/hover/0AZ.jpg");
+    });
+
+    $( "#az" ).mouseout(function(){
+        $(this).attr("src", "/images/work/grid/transparent/350x225_AZ.png");
+    });
+}); 
 
 $(document).ready(function() {
     $( "#hbo" ).mouseover(function(){
-        $(this).attr("src", "images/work/grid/hover/1HBO.jpg");
+        $(this).attr("src", "/images/work/grid/hover/1HBO.jpg");
     });
 
     $( "#hbo" ).mouseout(function(){
-        $(this).attr("src", "images/work/grid/transparent/350x225_HBO.png");
+        $(this).attr("src", "/images/work/grid/transparent/350x225_HBO.png");
     });
 });
 
 $(document).ready(function() {
     $( "#milk" ).mouseover(function(){
-        $(this).attr("src", "./images/work/grid/hover/2Milk.jpg");
+        $(this).attr("src", "/images/work/grid/hover/2Milk.jpg");
     });
 
     $( "#milk" ).mouseout(function(){
-        $(this).attr("src", "./images/work/grid/transparent/350x225_Milk.png");
+        $(this).attr("src", "/images/work/grid/transparent/350x225_Milk.png");
     });
 });
 
 $(document).ready(function() {
     $( "#tourneau" ).mouseover(function(){
-        $(this).attr("src", "images/work/grid/hover/3Tourneau.jpg");
+        $(this).attr("src", "/images/work/grid/hover/3Tourneau.jpg");
     });
 
     $( "#tourneau" ).mouseout(function(){
-        $(this).attr("src", "images/work/grid/transparent/350x225_Tourneau.png");
+        $(this).attr("src", "/images/work/grid/transparent/350x225_Tourneau.png");
     });
 });
 
 $(document).ready(function() {
     $( "#claritin" ).mouseover(function(){
-        $(this).attr("src", "images/work/grid/hover/4Claritin.jpg");
+        $(this).attr("src", "/images/work/grid/hover/4Claritin.jpg");
     });
 
     $( "#claritin" ).mouseout(function(){
-        $(this).attr("src", "images/work/grid/transparent/350x225_Claritin.png");
+        $(this).attr("src", "/images/work/grid/transparent/350x225_Claritin.png");
     });
 });
 
 $(document).ready(function() {
     $( "#coppertone" ).mouseover(function(){
-        $(this).attr("src", "images/work/grid/hover/5Coppertone.jpg");
+        $(this).attr("src", "/images/work/grid/hover/5Coppertone.jpg");
     });
 
     $( "#coppertone" ).mouseout(function(){
-        $(this).attr("src", "images/work/grid/transparent/350x225_Coppertone.png");
+        $(this).attr("src", "/images/work/grid/transparent/350x225_Coppertone.png");
     });
 });
 
 $(document).ready(function() {
     $( "#coppertone" ).mouseover(function(){
-        $(this).attr("src", "images/work/grid/hover/5Coppertone.jpg");
+        $(this).attr("src", "/images/work/grid/hover/5Coppertone.jpg");
     });
 
     $( "#coppertone" ).mouseout(function(){
-        $(this).attr("src", "images/work/grid/transparent/350x225_Coppertone.png");
+        $(this).attr("src", "/images/work/grid/transparent/350x225_Coppertone.png");
     });
 });
 
 $(document).ready(function() {
     $( "#floridalottery" ).mouseover(function(){
-        $(this).attr("src", "images/work/grid/hover/6FLottery.jpg");
+        $(this).attr("src", "/images/work/grid/hover/6FLottery.jpg");
     });
 
     $( "#floridalottery" ).mouseout(function(){
-        $(this).attr("src", "images/work/grid/transparent/350x225_FLottery.png");
+        $(this).attr("src", "/images/work/grid/transparent/350x225_FLottery.png");
     });
 });
 
 $(document).ready(function() {
     $( "#palmbay" ).mouseover(function(){
-        $(this).attr("src", "images/work/grid/hover/7PalmBay.jpg");
+        $(this).attr("src", "/images/work/grid/hover/7PalmBay.jpg");
     });
 
     $( "#palmbay" ).mouseout(function(){
-        $(this).attr("src", "images/work/grid/transparent/350x225_PalmBay.png");
+        $(this).attr("src", "/images/work/grid/transparent/350x225_PalmBay.png");
     });
 });
 
 $(document).ready(function() {
     $( "#itt" ).mouseover(function(){
-        $(this).attr("src", "images/work/grid/hover/8ITT.jpg");
+        $(this).attr("src", "/images/work/grid/hover/8ITT.jpg");
     });
 
     $( "#itt" ).mouseout(function(){
-        $(this).attr("src", "images/work/grid/transparent/350x225_ITT.png");
+        $(this).attr("src", "/images/work/grid/transparent/350x225_ITT.png");
     });
 });
 
 $(document).ready(function() {
     $( "#susemi" ).mouseover(function(){
-        $(this).attr("src", "images/work/grid/hover/9Susemi.jpg");
+        $(this).attr("src", "/images/work/grid/hover/9Susemi.jpg");
     });
 
     $( "#susemi" ).mouseout(function(){
-        $(this).attr("src", "images/work/grid/transparent/350x225_Susemi.png");
+        $(this).attr("src", "/images/work/grid/transparent/350x225_Susemi.png");
     });
 });
 
 $(document).ready(function() {
     $( "#backtotop" ).mouseover(function(){
-        $(this).attr("src", "images/work/grid/hover/backtotop.jpg");
+        $(this).attr("src", "/images/work/grid/hover/backtotop.jpg");
     });
 
     $( "#backtotop" ).mouseout(function(){
-        $(this).attr("src", "images/work/grid/backtotop.jpg");
+        $(this).attr("src", "/images/work/grid/backtotop.jpg");
     });
 });
 
